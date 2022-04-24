8c4cb25757ab4395d92a8095c2dbd3fd5c41f702
Marcin <zasadzkimarcin@gmail.com>
2022-04-24 15:28:26 +0100

    leet code remove duplicates loop version

--- src/__tests__/removeDuplicatesFromSortedArray.test.ts
+++ src/__tests__/removeDuplicatesFromSortedArray.test.ts
@@ -1,8 +1,13 @@
-import { removeDuplicates } from "../removeDuplicatesFromSortedArray";
+import { removeDuplicates, remDup } from "../removeDuplicatesFromSortedArray";
 
 it("removeDuplicates", () => {
   expect(removeDuplicates([1, 1, 2])).toEqual([1, 2]);
   expect(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])).toEqual([
     0, 1, 2, 3, 4,
   ]);
+});
+
+fit("remDup", () => {
+  expect(remDup([1, 1, 2])).toEqual([1, 2]);
+  expect(remDup([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])).toEqual([0, 1, 2, 3, 4]);
 });
